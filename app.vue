1c9f6c903ee50e24b6ed0a6e7b40b61fd62152e4
StrohXl <xaviermayora20@gmail.com>
2024-02-12 11:43:12 -0400

    changing scrollbar

--- app.vue
+++ app.vue
@@ -6,9 +6,13 @@
     </main>
   </div>
 </template>
-<style lang="postcss">
+<style >
 body {
   @apply dark:bg-gray-900 bg-gray-100/90 dark:text-white text-gray-800;
+}
+html{
+  scrollbar-color: #00c16a #1f2937;
+  scrollbar-width: thin;
 }
 h2 {
   @apply text-4xl 2xl:!text-5xl font-bold;
